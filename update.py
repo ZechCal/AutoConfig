6e4605761e537ed31bbcc8ad65891ac8851e5c1d
ZechCal <zxyworkspace@outlook.com>
2018-07-31 14:28:28 +0800

    0.0 alpha 1 fix

--- update.py
+++ update.py
@@ -2,15 +2,15 @@ import pip
 from subprocess import call
 from pip._internal.utils.misc import get_installed_distributions
 
-source = ""
+Source = ""
 
-sources = ["http://mirrors.aliyun.com/pypi/simple/","https://pypi.mirrors.ustc.edu.cn/simple/",\
+Sources = ["http://mirrors.aliyun.com/pypi/simple/","https://pypi.mirrors.ustc.edu.cn/simple/",\
 "https://pypi.tuna.tsinghua.edu.cn/simple/"]
 
 print("Please select a mirror, -1 to quit")
 
 i = 0
-for item in sources:
+for item in Sources:
     print(str(i) + " " + item)
     i += 1
 
@@ -18,16 +18,20 @@ i = input("Please input a value:")
 
 i = int(i)
 
-if i < 0 and i > len(sources):
-    source = " -i " + sources[i]
+#index should between 0 and length
+
+if i >= 0 and i < len(Sources):
+    Source = " -i " + Sources[i]
+elif i == -1:
+    exit()
 
 command = input(
-    "Please input a command or do nothing*Follow with pip description:")
+    "Please input a command or do nothing * Follow with pip description:")
 
 if command.strip() == '':
     for dist in get_installed_distributions():
         call(
-            "pip install --upgrade " + dist.project_name + source,
+            "pip install --upgrade " + dist.project_name + Source,
             shell=True)
 else:
-    call("pip install " + command +  source, shell=True)
+    call("pip install " + command +  Source, shell=True)
